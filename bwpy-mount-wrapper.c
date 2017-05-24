239a2cf3a91ae5769df41540e134fd20961ba0bd
Colin MacLean <cmaclean@illinois.edu>
2017-05-24 18:58:26 -0500

    versions

--- bwpy-mount-wrapper.c
+++ bwpy-mount-wrapper.c
@@ -2,6 +2,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <fnmatch.h>
 #include <getopt.h>
 #include <grp.h>
 #include <libgen.h>
@@ -14,18 +15,184 @@
 #include <sys/file.h>
 #include <sys/mount.h>
 #include <sys/stat.h>
+#include <sys/syscall.h>
 #include <sys/sysmacros.h>
 #include <sys/types.h>
 #include <linux/limits.h>
 #include <linux/loop.h>
 
-#define IMAGE "/home/colin/test-symlink2.ext4"
+#define init_module(mod, len, opts) syscall(__NR_init_module, mod, len, opts)
+
+#define COLOR_NC   "\e[0m"
+#define COLOR_CYAN "\e[36m"
+#define COLOR_RED  "\e[31m"
+
+#define IMAGE_DEFAULT "/home/colin/bwpy.img"
+#define IMAGE_VERSIONED "/home/colin/bwpy-%s.img"
 #define IMAGE_DIR "/home/colin"
+#define IMAGE_PREFIX "bwpy"
+#define IMAGE_SUFFIX ".img"
 #define MOUNTPOINT "/sw/bw/bwpy"
 #define IMAGE_TYPE "ext4"
-#define MAINT_GROUP "bw_seas"
+#define MAINT_GROUP "colin"
+#define LOOP_NAME "loop"
+//#define LOOP_KO "/path/to/loop.ko"
+#define LOOP_KO "/lib/modules/4.10.4-gentoo-1-desktop/video/nvidia.ko"
 
 int maint = 0;
+
+const char* filename_to_version(char *filename) {
+    size_t len = strlen(filename);
+    if (len < strlen(IMAGE_PREFIX "-" IMAGE_SUFFIX))
+        return "default";
+    filename[len-strlen(IMAGE_SUFFIX)] = 0;
+    filename += strlen(IMAGE_PREFIX) + 1;
+    return filename;
+}
+
+int filter_versions(const struct dirent *ent) {
+    int ret = fnmatch(IMAGE_PREFIX "*" IMAGE_SUFFIX,ent->d_name,0);
+    return !ret;
+}
+
+int list_versions(void) {
+    struct dirent **namelist;
+    char path[PATH_MAX];
+    char rl[PATH_MAX];
+    char finalpath[PATH_MAX];
+    int n, found = 0;
+
+    n = scandir(IMAGE_DIR, &namelist, filter_versions, versionsort);
+    if (n < 0) {
+        fprintf(stderr,"Error: No images found.\n");
+        return -1;
+    }
+    else {
+        printf("Versions:\n");
+        while (n--) {
+            snprintf(path,PATH_MAX,"%s/%s",IMAGE_DIR,namelist[n]->d_name);
+            struct stat st;
+            if (lstat(path,&st) < 0)
+                continue;
+            if (S_ISLNK(st.st_mode)) {
+                if (stat(path,&st) < 0)
+                    continue;
+                memset(rl,0,PATH_MAX);
+                if (readlink(path, rl, PATH_MAX) < 0)
+                    continue;
+                if (realpath(path,finalpath) == NULL)
+                    continue;
+                if (stat(finalpath,&st) < 0)
+                    continue;
+                if (!(st.st_mode & 04))
+                    continue;
+                ++found;
+                printf(COLOR_CYAN "%s" COLOR_NC " -> %s\n",filename_to_version(namelist[n]->d_name),filename_to_version(basename(rl)));
+            } else {
+                if (!(st.st_mode & 04))
+                    continue;
+                ++found;
+                printf("%s\n", filename_to_version(namelist[n]->d_name));
+            }
+            free(namelist[n]);
+        }
+        free(namelist);
+    }
+    if (found == 0)
+    printf("NONE!\n");
+    return 0;
+}
+
+//Get the versioned image name
+//Ensure that the image name is actually in the good directory
+//This is to prevent %s.img being ../../exploit
+const char *versioned_image(const char* version_string) {
+    char good_realdir[PATH_MAX]; 
+    char suspect_path[PATH_MAX];
+    char suspect_realpath[PATH_MAX];
+    char suspect_realdir[PATH_MAX];
+    const char* clean_path = NULL;
+
+    if (realpath(IMAGE_DIR, good_realdir) == NULL) {
+        fprintf(stderr,"Error: failed to get real path of image directory: %s %s\n",IMAGE_DIR,strerror(errno));
+        return NULL;
+    }
+    
+    snprintf(suspect_path,PATH_MAX,IMAGE_VERSIONED,version_string);
+
+    
+    if (realpath(suspect_path, suspect_realpath) == NULL) {
+        fprintf(stderr,"Error: failed to get real path of requested image %s: %s\n",suspect_path,strerror(errno));
+        return NULL;
+    }
+
+    strncpy(suspect_realdir,suspect_realpath,PATH_MAX);
+
+    if (strcmp(good_realdir,dirname(suspect_realdir)) == 0)
+        clean_path = suspect_realpath;
+    else 
+        fprintf(stderr,"Error: Something fishy is going on here. %s != %s\n",dirname(suspect_realpath),good_realdir);
+    
+    return clean_path;
+}
+
+int setup_module(const char* name, const char* ko_file) {
+    FILE *f;
+    char line[4096];
+    int found = 0;
+    size_t ko_size;
+    ssize_t read_size;
+    void *ko_image;
+
+    if ((f = fopen("/proc/modules","r")) == NULL) {
+        fprintf(stderr,"Error: Cannot open /proc/modules: %s!\n",strerror(errno));
+        return -1;
+    }
+
+    while (fgets(line, sizeof(line), f)) {
+        char *tok;
+		tok = strtok(line, " \t");
+
+        if (strcmp(name, tok) == 0) {
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        int fd;
+        
+        if ((fd = open(ko_file, O_RDONLY)) < 0) {
+            fprintf(stderr,"Error: Cannot open %s: %s!\n",ko_file,strerror(errno));
+            return -1;
+        }
+
+        struct stat st;
+        if (fstat(fd, &st) < 0) {
+            fprintf(stderr,"Error: Cannot stat %s: %s!\n",ko_file,strerror(errno));
+            return -1;
+        }
+
+        ko_size  = st.st_size;
+        ko_image = malloc(ko_size);
+
+        if ((read_size = read(fd, ko_image, ko_size)) < ko_size) {
+            if (read_size < 0)
+                fprintf(stderr,"Error: Error reading %s: %s!\n",ko_file,strerror(errno));
+            else 
+                fprintf(stderr,"Error: %s smaller than expected. %zu < %zu.\n", ko_file, (size_t) read_size, ko_size);
+            return -1;
+        }
+
+        close(fd);
+        if (init_module(ko_image, ko_size, "max_loop=256") != 0) {
+            fprintf(stderr,"Error: Error inserting %s: %s!\n",ko_file,strerror(errno));
+            return -1;
+        }
+    }
+
+    return 0;
+}
 
 const char *loop_dev_num(const unsigned char device_num) {
     static char buffer[32];
@@ -47,7 +214,11 @@ const char *backing_file(const unsigned char device_num) {
     if ((fd = open(sys_backing_file, O_RDONLY)) < 0)
         return NULL;
 
-    if ((len = read(fd,res,PATH_MAX)) == 0) {
+    if ((len = read(fd,res,PATH_MAX)) <= 0) {
+        if (len < 0) {
+            fprintf(stderr,"Error: Error reading %s: %s!\n",sys_backing_file,strerror(errno));
+            return NULL;
+        }
         close(fd);
         return NULL;
     }
@@ -83,7 +254,7 @@ int setup_loop_dev(const char *image_path) {
     int err = 0;
 
     if (realpath(image_path,real_image_path) == NULL) {
-        fprintf(stderr,"Error: failed to get real path of %s\n",image_path);
+        fprintf(stderr,"Error: failed to get real path of %s: %s\n",image_path,strerror(errno));
         return -1;
     }
 
@@ -183,17 +354,47 @@ error_noprint:
     return -1;
 }
 
+void drop_priv_perm(uid_t uid, gid_t gid) {
+    //Permanently drop privileges
+    //
+    //setresgid/setresuid makes it more obvious that we are 
+    //dropping real, effective, and saved uid/gid privileges
+    //vs setuid(getuid()) and setgid(getuid())
+    if (setresgid(gid,gid,gid) != 0) {
+        fprintf(stderr,"Error: Error dropping group privileges: %s!\n",strerror(errno));
+        abort();
+    }
+    if (setresuid(uid,uid,uid) != 0) {
+        fprintf(stderr,"Error: Error dropping privileges: %s!\n",strerror(errno));
+        abort();
+    }
+
+    //Check that privileges were dropped
+    uid_t check_ru, check_eu, check_su;
+    gid_t check_rg, check_eg, check_sg;
+    if (getresuid (&check_ru, &check_eu, &check_su) != 0
+            || check_ru != uid || check_eu != uid || check_su != uid) {
+        fprintf(stderr,"Error: Privileges were not dropped!\n");
+        abort ();
+    }
+  
+    if (getresgid (&check_rg, &check_eg, &check_sg) != 0
+            || check_rg != gid || check_eg != gid || check_sg != gid) {
+        fprintf(stderr,"Error: Group privileges were not dropped!\n");
+        abort ();
+    }
+}
+
 int main(int argc, char *argv[])
 {
     char wrappername[NAME_MAX];
     const char *program;
     char **program_args;
     int loopdev;
-    int use_shell = 0; 
-    int shift = 1;
     char version[64];
     int has_version = 0;
     char user_shell[PATH_MAX];
+    const char *image_name = IMAGE_DEFAULT; 
 
     //Get current real and effective privileges
     gid_t gid = getgid();
@@ -202,6 +403,16 @@ int main(int argc, char *argv[])
     uid_t euid = geteuid();
     gid_t sgid = egid;
     uid_t suid = euid;
+
+    if (egid != 0) {
+        fprintf(stderr,"Error: Not a root suid binary!\n");
+        return -1;
+    }
+
+    if (setup_module(LOOP_NAME,LOOP_KO) < 0) {
+        fprintf(stderr,"Error: No loop device support!\n");
+        return -1;
+    }
 
     //Temporarily drop permissions
     if (setegid(gid)) {
@@ -222,12 +433,13 @@ int main(int argc, char *argv[])
         static struct option long_options[] = {
             { "maintenance",   no_argument,       0,  'm' },
             { "image-version", required_argument, 0,  'v' },
+            { "list",          no_argument,       0,  'l' },
             { "version",       no_argument,       0,  'V' },
             { "help",          no_argument,       0,  'h' },
             { 0,               0,                 0,  0   }
         };
 
-        c = getopt_long(argc, argv, "mv:h", long_options, &option_index);
+        c = getopt_long(argc, argv, "mlv:hV", long_options, &option_index);
 
         if (c == -1)
             break;
@@ -241,6 +453,11 @@ int main(int argc, char *argv[])
                 has_version = 1;
                 strncpy(version,optarg,64);
                 break;
+
+            case 'l':
+                drop_priv_perm(uid,gid);
+                list_versions();
+                return 0;
 
             case 'h':
                 printf("Usage: %s [-mhv] [--image-version version] [--maintenance] [--] [program [args...]]\n",wrappername);
@@ -272,9 +489,10 @@ int main(int argc, char *argv[])
         }
         strncpy(user_shell,tmp,PATH_MAX);
         program = user_shell;
+        program_args = NULL;
     }
 
-    if (maint) {
+    if (maint && uid != 0) {
         struct passwd *pwinfo;
         struct group *maintgrpinfo;
         int i, ngroups = 1;
@@ -300,11 +518,13 @@ int main(int argc, char *argv[])
             retry = realloc(groups,ngroups*sizeof(gid_t));
             if (retry == NULL) {
                 fprintf(stderr,"Error: Cannot allocate memory!\n");
+                free(groups);
                 return -1;
             } else {
                 groups = retry;
             }
             if (getgrouplist(argv[1], pwinfo->pw_gid, groups, &ngroups) == -1) {
+                free(groups);
                 fprintf(stderr,"Error: Error getting user's groups!\n");
                 return -1;
             }    
@@ -318,9 +538,17 @@ int main(int argc, char *argv[])
             fprintf(stderr, "Error: You must be a member of the %s group to mount read-write!\n",MAINT_GROUP);
             return -1;
         }
+        free(groups);
     }
 
-    if ((loopdev = setup_loop_dev(IMAGE)) < 0) {
+    //Do this after parsing args, in case --help or --version are specified,
+    //which take priority
+    if (has_version) {
+        if ((image_name = versioned_image(version)) == NULL)
+            return -1;
+    }
+
+    if ((loopdev = setup_loop_dev(image_name)) < 0) {
         fprintf(stderr,"Error setting up loop device /dev/loop%hhu!\n",loopdev);
         return -1;
     }
@@ -334,80 +562,47 @@ int main(int argc, char *argv[])
         fprintf(stderr,"Error: Cannot regain group privileges: %s!\n",strerror(errno));
         return -1;
     }
-    
-    if (euid == 0) {
-        if (unshare(CLONE_NEWNS) != 0) {
-            fprintf(stderr,"Error: Cannot create mount namespace: %s!\n",strerror(errno));
-            return -1;
-        }
 
-        if (mount("none", "/", NULL, MS_REC | MS_PRIVATE, NULL) == -1) {
-            fprintf(stderr,"Error: Cannot give / subtree private mount propagation: %s!\n",strerror(errno));
-            return -1;
-        }
+    if (unshare(CLONE_NEWNS) != 0) {
+        fprintf(stderr,"Error: Cannot create mount namespace: %s!\n",strerror(errno));
+        return -1;
+    }
 
-        const char* loop_dev_file = loop_dev_num(loopdev);
-        unsigned long mountflags = MS_NOSUID | MS_NODEV | MS_NOATIME;
-        if (!maint)
-            mountflags |= MS_RDONLY;
+    if (mount("none", "/", NULL, MS_REC | MS_PRIVATE, NULL) == -1) {
+        fprintf(stderr,"Error: Cannot give / subtree private mount propagation: %s!\n",strerror(errno));
+        return -1;
+    }
+
+    const char* loop_dev_file = loop_dev_num(loopdev);
+    unsigned long mountflags = MS_NOSUID | MS_NODEV | MS_NOATIME;
+    if (!maint)
+        mountflags |= MS_RDONLY;
             
-        if (mount(loop_dev_file, MOUNTPOINT, IMAGE_TYPE, mountflags, "") < 0){
-            fprintf(stderr,"Error: Cannot mount image: %s!\n",strerror(errno));
-            return -1;
-        }
+    if (mount(loop_dev_file, MOUNTPOINT, IMAGE_TYPE, mountflags, "") < 0){
+        fprintf(stderr,"Error: Cannot mount image: %s!\n",strerror(errno));
+        return -1;
+    }
 
-        //Set loop device to detach automatically once last mount is unmounted
-        int loopfd;
-        if ((loopfd = open(loop_dev_file, O_RDWR)) < 0) {
-            fprintf(stderr,"Error: Error opening loop device: %s!\n",strerror(errno));
-            return -1;
-        }
+    //Set loop device to detach automatically once last mount is unmounted
+    int loopfd;
+    if ((loopfd = open(loop_dev_file, O_RDWR)) < 0) {
+        fprintf(stderr,"Error: Error opening loop device: %s!\n",strerror(errno));
+        return -1;
+    }
 
-        struct loop_info64 loopinfo64;
-        memset(&loopinfo64, 0, sizeof(loopinfo64));
-        loopinfo64.lo_flags = LO_FLAGS_AUTOCLEAR;
+    struct loop_info64 loopinfo64;
+    memset(&loopinfo64, 0, sizeof(loopinfo64));
+    loopinfo64.lo_flags = LO_FLAGS_AUTOCLEAR;
     
-        if (ioctl(loopfd, LOOP_SET_STATUS64, &loopinfo64) < 0) {
-            fprintf(stderr,"Error: Error setting LO_FLAGS_AUTOCLEAR: %s!\n",strerror(errno));
-            close(loopfd);
-            return -1;
-        }
-
+    if (ioctl(loopfd, LOOP_SET_STATUS64, &loopinfo64) < 0) {
+        fprintf(stderr,"Error: Error setting LO_FLAGS_AUTOCLEAR: %s!\n",strerror(errno));
         close(loopfd);
-
-    } else {
-        fprintf(stderr,"Error: Not a root suid binary!\n");
         return -1;
     }
+
+    close(loopfd);
    
-    //Permanently drop privileges
-    //
-    //setresgid/setresuid makes it more obvious that we are 
-    //dropping real, effective, and saved uid/gid privileges
-    //vs setuid(getuid()) and setgid(getuid())
-    if (setresgid(gid,gid,gid) != 0) {
-        fprintf(stderr,"Error: Error dropping group privileges: %s!\n",strerror(errno));
-        return -1;
-    }
-    if (setresuid(uid,uid,uid) != 0) {
-        fprintf(stderr,"Error: Error dropping privileges: %s!\n",strerror(errno));
-        return -1;
-    }
-
-    //Check that privileges were dropped
-    uid_t check_ru, check_eu, check_su;
-    gid_t check_rg, check_eg, check_sg;
-    if (getresuid (&check_ru, &check_eu, &check_su) != 0
-            || check_ru != uid || check_eu != uid || check_su != uid) {
-        fprintf(stderr,"Error: Privileges were not dropped!\n");
-        abort ();
-    }
-  
-    if (getresgid (&check_rg, &check_eg, &check_sg) != 0
-            || check_rg != gid || check_eg != gid || check_sg != gid) {
-        fprintf(stderr,"Error: Group privileges were not dropped!\n");
-        abort ();
-    }
+    drop_priv_perm(uid,gid);
 
     if (execvp(program,program_args) < 0) {
         fprintf(stderr,"Error: Error executing %s: %s!\n",program,strerror(errno));
